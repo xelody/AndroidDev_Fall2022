15de4f55a4f7561b833535ba9fc5225e5f057b7b
xelody <melodypxin@gmail.com>
2022-09-07 21:54:30 -0700

    Added an onClickListener to display a Toast with my name and email.

--- app/src/main/java/edu/northeastern/numad22fa_peiyaoxin/MainActivity.java
+++ app/src/main/java/edu/northeastern/numad22fa_peiyaoxin/MainActivity.java
@@ -8,13 +8,17 @@ import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    Button about_btn;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Button about_btn = (Button) findViewById(R.id.about_btn);
 
+        about_btn.setOnClickListener(view ->
+                Toast.makeText(MainActivity.this, "Peiyao Xin\nxin.pe@northeastern.edu",
+                        Toast.LENGTH_LONG).show());
     }
 }
