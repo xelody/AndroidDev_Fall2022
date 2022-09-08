77a159f30ee8e6416b6c10e09c9b7836ba0bac91
xelody <melodypxin@gmail.com>
2022-09-07 21:48:43 -0700

    Added "About Me" button to activity_main.xml and change the font size of "Hello World!" text

--- app/src/main/java/edu/northeastern/numad22fa_peiyaoxin/MainActivity.java
+++ app/src/main/java/edu/northeastern/numad22fa_peiyaoxin/MainActivity.java
@@ -3,12 +3,18 @@ package edu.northeastern.numad22fa_peiyaoxin;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    Button about_btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
 }
