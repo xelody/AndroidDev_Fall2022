a7e58c2c1faa1c41563ec3ba3cea0603a551214c
xelody <melodypxin@gmail.com>
2022-09-21 20:38:23 -0700

    Added onClickListener for each button on ClickyActivity to display the corresponding button name int the textview

--- app/src/main/java/edu/northeastern/numad22fa_peiyaoxin/ClickyActivity.java
+++ app/src/main/java/edu/northeastern/numad22fa_peiyaoxin/ClickyActivity.java
@@ -3,6 +3,9 @@ package edu.northeastern.numad22fa_peiyaoxin;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class ClickyActivity extends AppCompatActivity {
 
@@ -10,5 +13,28 @@ public class ClickyActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_clicky);
+
+        Button btnA = findViewById(R.id.buttonA);
+        Button btnB = findViewById(R.id.buttonB);
+        Button btnC = findViewById(R.id.buttonC);
+        Button btnD = findViewById(R.id.buttonD);
+        Button btnE = findViewById(R.id.buttonE);
+        Button btnF = findViewById(R.id.buttonF);
+
+        TextView pressedTV = (TextView)  findViewById(R.id.TV_Pressed);
+        String textString = pressedTV.getText().toString();
+
+        btnA.setOnClickListener(view ->
+                pressedTV.setText(textString.replace("-", "A")));
+        btnB.setOnClickListener(view ->
+                pressedTV.setText(textString.replace("-", "B")));
+        btnC.setOnClickListener(view ->
+                pressedTV.setText(textString.replace("-", "C")));
+        btnD.setOnClickListener(view ->
+                pressedTV.setText(textString.replace("-", "D")));
+        btnE.setOnClickListener(view ->
+                pressedTV.setText(textString.replace("-", "E")));
+        btnF.setOnClickListener(view ->
+                pressedTV.setText(textString.replace("-", "F")));
     }
 }
