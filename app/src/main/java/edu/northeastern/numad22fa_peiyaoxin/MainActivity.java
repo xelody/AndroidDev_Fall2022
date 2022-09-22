3814f0ee7fcc54d603e40973839b40e935be15a9
xelody <melodypxin@gmail.com>
2022-09-21 20:11:38 -0700

    Added onClickListener to open ClickyActivity

--- app/src/main/java/edu/northeastern/numad22fa_peiyaoxin/MainActivity.java
+++ app/src/main/java/edu/northeastern/numad22fa_peiyaoxin/MainActivity.java
@@ -2,6 +2,7 @@ package edu.northeastern.numad22fa_peiyaoxin;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -15,10 +16,19 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button about_btn = (Button) findViewById(R.id.about_btn);
+        Button about_btn = findViewById(R.id.about_btn);
 
         about_btn.setOnClickListener(view ->
                 Toast.makeText(MainActivity.this, "Peiyao Xin\nxin.pe@northeastern.edu",
                         Toast.LENGTH_LONG).show());
-    }
+
+        Button clickyBtn = (Button) findViewById(R.id.clicky_btn);
+
+        clickyBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, ClickyActivity.class));
+            }
+        });
+     }
 }
